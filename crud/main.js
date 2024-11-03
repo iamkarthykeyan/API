e11a67af8f26f72297a2d172b6856a338f30b13d
Karthikeyan <j.karthikeyandev@gmail.com>
2024-11-03 20:37:20 +0530

    http methods

--- crud/main.js
+++ crud/main.js
@@ -3,10 +3,32 @@ import express from 'express';
 const app = express();
 const PORT = 5000;
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.json({ msg: "Hey there! Whatsapp?" });
-})
+});
 
-app.listen(PORT,()=>{
+// Http methods
+
+// READ
+app.get('/items', () => {
+
+});
+
+// CREATE
+app.post('/items', () => {
+
+});
+
+// UPDATE
+app.put('/items/:id', () => {
+
+});
+
+// DELETE
+app.delete('/items/:id', () => {
+
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`);
 });
